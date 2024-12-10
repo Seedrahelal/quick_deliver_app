b127eccddb8de19108522310b03f0feec7ae46cb
Seedrahelal <sedeahelal16@gmail.com>
2024-12-10 09:25:00 +0300

    add action to open the gallery

--- lib/widgets/pick_image_widget.dart
+++ lib/widgets/pick_image_widget.dart
@@ -1,48 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PickImageWidget extends StatelessWidget {
-  const PickImageWidget({
-    super.key,
-  });
+  const PickImageWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 160,
-        height: 160,
-        child: CircleAvatar(
-          backgroundImage:
-              const AssetImage('assets/images/pick_photo.jpg'),
-          child: Stack(
-            children: [
-              Positioned(
-                bottom: 5,
-                right: 5,
-                child: GestureDetector(
-                  onTap: () async {},
-                  child: Container(
-                    height: 55,
-                    width: 55,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 116, 33, 51),
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 231, 230, 230),
-                          width: 3),
-                      borderRadius: BorderRadius.circular(30),
+      width: 160,
+      height: 160,
+      child: CircleAvatar(
+        backgroundImage: const AssetImage('assets/images/pick_photo.jpg'),
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: 5,
+              right: 5,
+              child: GestureDetector(
+                onTap: () async {
+                  final pickedFile = await ImagePicker()
+                      .pickImage(source: ImageSource.gallery);
+                },
+                child: Container(
+                  height: 55,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 116, 33, 51),
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 231, 230, 230),
+                      width: 3,
                     ),
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: const Icon(
-                        Icons.camera_alt_sharp,
-                        color: Color.fromARGB(255, 231, 230, 230),
-                        size: 30,
-                      ),
-                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Icon(
+                    Icons.camera_alt_sharp,
+                    color: Color.fromARGB(255, 231, 230, 230),
+                    size: 30,
                   ),
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
